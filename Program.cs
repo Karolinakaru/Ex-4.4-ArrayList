3948252744e408b59df23243d30a2393bd9e90bc
singhet <singhet@SCSWL4072>
2024-02-06 06:43:54 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        //Program adds to the collection cities:then sorts them alphabetically.
+        var citiesList = new ArrayList();
+
+        citiesList.Add("Gdańsk");
+        citiesList.Add("Warszawa");
+        citiesList.Add("Katowice");
+        citiesList.Add("Poznań");
+        citiesList.Add("Kraków");
+        citiesList.Add("Rzeszów");
+
+        citiesList.Sort();
+        Console.WriteLine("Cities sorted alphabetically are:");
+        foreach (var city in citiesList) 
+        {
+            Console.WriteLine(city);
+        }
+        Console.ReadLine();
+    }
+}
+
+
+
